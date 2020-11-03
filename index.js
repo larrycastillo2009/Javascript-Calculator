7fc7f1f8ebb6ad95095091856a2ae8a80bfa8a66
Larry Castillo <larrycastillo2009@gmail.com>
2020-11-02 22:41:34 -0600

    clear functionality

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 "use strict";
 
 function dis(val) {
-    document.getElementById("fNum").value += val;
+    if(document.getElementById("operator").value != ""){
+        document.getElementById("sNum").value += val;
+    }else {
+        document.getElementById("fNum").value += val;
+    }
 }
 
 function op(val) {
-    document.getElementById("operator").value += val;
+    document.getElementById("operator").value = val;
+}
+
+function clearStr() {
+    document.getElementById("operator").value = '';
+    document.getElementById("fNum").value = '';
+    document.getElementById("sNum").value = '';
 }
