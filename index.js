97747de2171dd1d4855bebb0a807ab6aa9c10a42
Larry Castillo <larrycastillo2009@gmail.com>
2020-11-03 14:03:09 -0600

    dot functionality complete

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 "use strict";
 
 function dis(val) {
+    const dot = document.getElementById("thedot")
+
     if(document.getElementById("operator").value != ""){
-        document.getElementById("sNum").value += val;
+        dot.disabled = false;
+        if (val == "."){
+            document.getElementById("sNum").value += val;
+            dot.disabled =true;
+        }else {
+            document.getElementById("sNum").value += val;
+        }
     }else {
-        document.getElementById("fNum").value += val;
+        if (val == "."){
+            document.getElementById("fNum").value += val;
+            dot.disabled =true;
+        }else {
+            document.getElementById("fNum").value += val;
+        }
     }
 }
 
