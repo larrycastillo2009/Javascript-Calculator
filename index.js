511fa1c49acc358265353f9bb979d8c13202c0ff
Larry Castillo <larrycastillo2009@gmail.com>
2020-11-30 14:34:06 -0600

    code cleanup and comments added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 "use strict";
 
+// Variable for number memory button/functionality
 var numMemory = "";
 
+// Function for displaying numbers on screen
 function dis(val) {
     const dot = document.getElementById("thedot")
 
@@ -46,16 +48,19 @@ function dis(val) {
     }
 }
 
+// Function for displaying operator on screen
 function op(val) {
     document.getElementById("operator").value = val;
 }
 
+// Function clearing screen
 function clearStr() {
     document.getElementById("operator").value = '';
     document.getElementById("fNum").value = "0";
     document.getElementById("sNum").value = '';
 }
 
+// Function for solving and displaying solution to screen
 function solution() {
     var firstNum = document.getElementById("fNum").value;
     var secondNum = document.getElementById("sNum").value;
@@ -73,6 +78,7 @@ function solution() {
 
 }
 
+// Function for computating the square root
 function sqroot() {
     var firstNum = document.getElementById("fNum").value;
     var answer = Math.sqrt(firstNum);
@@ -80,6 +86,7 @@ function sqroot() {
     dis(answer);
 }
 
+// Function for converting a number into a percentage
 function numPercentage() {
     if (document.getElementById("operator").value != ""){
         var dNum = document.getElementById("sNum").value
@@ -90,6 +97,7 @@ function numPercentage() {
     }
 }
 
+// Function for storing/recalling a number into calculator memory
 function MRC() {
     if(document.getElementById("operator").value != ""){
         document.getElementById("sNum").value = numMemory;
@@ -98,6 +106,7 @@ function MRC() {
     }
 }
 
+// Function for subtracting a number from the number stored in memory
 function negM() {
     if (document.getElementById("operator").value != "") {
         var disNum = document.getElementById("sNum").value;
@@ -118,6 +127,7 @@ function negM() {
     }
 }
 
+// Function for adding a number from the number stored in memory
 function plusM() {
     if (document.getElementById("operator").value != "") {
         var disNum = document.getElementById("sNum").value;
@@ -139,40 +149,6 @@ function plusM() {
     }
 }
 
-function compressedString(message) {
-    var newStr = "";
-    var counter = 0;
-    for(var i = 0; i < message.length; i++){
-        var newChar = message.charAt(i);
-        console.log(newChar)
-        var newerChar = message.charAt(i+1)
-        console.log(newerChar)
-       if(newChar === newerChar){
-           counter++;
-       }else {
-           counter++;
-           console.log(counter)
-           newStr = newStr + newChar;
-           if (counter > 1) {
-               newStr = newStr + counter;
-           }
-           counter = 0;
-       }
 
-    }
-    return newStr;
-}
-console.log(compressedString("aabb"));
-
-$(document).ready(function () {
-    var expanded = false;
-    $("#drawer-handle").click(function () {
-        if (expanded = !expanded) {
-            $("#drawer-content").animate({ "margin-right": 0 },    "slow");
-        } else {
-            $("#drawer-content").animate({ "margin-right": -300 }, "slow");
-        }
-    });
-});
 
 
